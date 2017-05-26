a533b59dab885a48dddc3f8e282ead3bf9303414
Ashley Williams <ashley666ashley@gmail.com>
2017-05-26 11:34:15 -0400

    feat(errors): add custom error to stack trace and test for it

--- test/potoroo.js
+++ test/potoroo.js
@@ -1,6 +1,7 @@
 /* global describe:true, it:true */
 
 const Password = require('../')
+const UnsafePassword = require('../lib/errors')
 const assert = require('chai').assert
 
 describe('potoroo', function () {
@@ -10,6 +11,8 @@ describe('potoroo', function () {
     assert.isFalse(Password(password, username).isSafe)
     assert.property(Password(password, username), 'err')
     assert.instanceOf(Password(password, username).err, Error)
+    assert.instanceOf(Password(password, username).err, UnsafePassword.Username)
+    assert.isTrue(Password(password, username).err.message.length > 0)
   })
 
   it('returns false and an error if the password is less than seven characters', function () {
@@ -18,14 +21,18 @@ describe('potoroo', function () {
     assert.isFalse(Password(password, username).isSafe)
     assert.property(Password(password, username), 'err')
     assert.instanceOf(Password(password, username).err, Error)
+    assert.instanceOf(Password(password, username).err, UnsafePassword.Short)
+    assert.isTrue(Password(password, username).err.message.length > 0)
   })
 
   it('returns false and an error  if the password exists in the rockyou password list', function () {
-    const password = '12345'
+    const password = '123456789'
     const username = 'rockyou'
     assert.isFalse(Password(password, username).isSafe)
     assert.property(Password(password, username), 'err')
     assert.instanceOf(Password(password, username).err, Error)
+    assert.instanceOf(Password(password, username).err, UnsafePassword.Common)
+    assert.isTrue(Password(password, username).err.message.length > 0)
   })
 
   it('returns true and no error if it passes all rules', function () {
