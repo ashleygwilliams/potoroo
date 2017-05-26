f369805ec2efaec74dd39b6c692fe70b3b5cef75
Ashley Williams <ashley666ashley@gmail.com>
2017-05-26 12:37:47 -0400

    fix(error): do not put html in error message

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function checkPassword (pw, username) {
     const msg = 'The provided password is too short. Please pick a password longer than 7 characters.'
     return { isSafe: false, err: new UnsafePassword.Short(msg) }
   } else if (unsafePasswordList.indexOf(pw) !== -1) {
-    const msg = 'The provided password is too common. Please pick a more unique password. See <a href="https://github.com/ashleygwilliams/rockyou/blob/master/data/70.txt">this list of too common passwords</a>.'
+    const msg = 'The provided password is too common. Please pick a more unique password.'
     return { isSafe: false, err: new UnsafePassword.Common(msg) }
   } else {
     return { isSafe: true }
